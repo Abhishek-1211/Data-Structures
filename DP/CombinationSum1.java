0b1d26a16449767bad9591bdcc270c089cb2db96
Abhishek kumar <abhishek.redefiningsimplicity@gmail.com>
2025-05-08 21:45:08 +0530

    feat: added new DP solution for CombinationSum problem and updated Readme

---
+++ DP/CombinationSum1.java
@@ -0,0 +1,65 @@
+package DP;
+
+import java.util.*;
+
+import static java.io.IO.print;
+import static java.io.IO.println;
+
+/*
+You are required to find all possible combination to make 'N' as sum of 1,3,5.Two solutions are different
+only when count of any one element is different
+e.g if N=6 then no. of  possible combinations = 4
+[1,1,1,1,1,1]
+[1,1,1,3] is same as [1,1,3,1],[1,3,1,1],[3,1,1,1]
+[3,3]
+[1,5] is same as [5,1]
+ */
+public class CombinationSum1 {
+
+    void main(){
+        int[] nums = {1,3,4};
+        int target = 7;
+
+        int ans = combinationSum(nums,target);
+        println(ans);
+    }
+
+    int combinationSum(int[] nums, int target) {
+        HashMap<List<Integer>,Integer> dp = new HashMap<>();
+        return combinationSumUtil(nums,nums.length-1,target,dp);
+//        int ans = 0;
+//        for(Map.Entry entrySet:dp.entrySet()){
+//            print(entrySet+" ");
+//        }
+//
+//        return ans;
+    }
+
+    int combinationSumUtil(int[] nums, int i, int target, HashMap<List<Integer>, Integer> dp) {
+
+        if(target==0)
+            return 1;
+
+        if(i<0)
+            return 0;
+
+        if(target<nums[i])
+            return combinationSumUtil(nums,i-1,target,dp);
+
+        int include = combinationSumUtil(nums,i,target-nums[i],dp);
+        int exclude = combinationSumUtil(nums,i-1,target,dp);
+        //int[]pair= new int[]{i,target};
+
+
+        if(!dp.containsKey(Arrays.asList(i,target)))
+            dp.put(Arrays.asList(i,target),include+exclude);
+//
+//        if(target>0 && !dp.containsKey(Arrays.asList(i,target)))
+//            return combinationSumUtil(nums,i,target,dp);
+
+        println("include "+nums[i]+ " "+include);
+        println("exclude "+nums[i]+ " "+exclude);
+        //return include+exclude;
+        return dp.get(Arrays.asList(i,target));
+    }
+}
--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # Data Structures and Algorithms
- - This repo contains solutions to leetcode questions as i continue to practice.
+ - This repo contains solutions to leetcode questions as I continue to practice.
  - I will try to post  atleast 2-3 questions on daily basis.
- - In this repo i have also tried latest fetaures of java like instance main methods-whereby main method acts as entry point of java program.
- - Topics covered - Arrays, linkedlist,Stack,Queue, Trees, Graph,Dynamic Programming, etc..
+ - In this repo i have also tried latest fetaures of java like instance main methods- 
+   whereby main method acts as entry point of java program.This is currently in DP programs
+ - Topics covered - Arrays, linkedlist,Stack,Queue, Trees, Graph,Dynamic Programming, etc.
 
-## Arrays-Problem List
+## Array-Problem List
 1. find maximum and minimun in an array
 2. find maximum and minimun in an array-single pass/loop
 2. find 2 max elements in a single pass
 3. reverse the array/reverse only vowels
-4. fizzbuzz
+4. Intersection of Arrays
 5. merge sorted array
 6. find duplicate in array
 7. count freqeuncy of each element
-8. 
+8. fizzbuzz
 
 ## LinkedList-Problem List
 - To be updated soon
@@ -26,4 +27,5 @@
 2. unique paths[62]
 3. Minimum path sum[64]
 4. House Robber[198]
-5. Permutation Sum[Tutort Academy]
+5. Permutation Sum
+6. combination Sum-I
